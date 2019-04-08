906adb3fb3cc6b2972a2c278fdd327b631c01654
GalGog <galynna00@gmail.com>
2019-04-08 15:14:44 +0100

    react Mdl

--- src/App.js
+++ src/App.js
@@ -1,45 +1,41 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import { Header, Menu, DropdownMenu, Segment, } from 'semantic-ui-react';
-
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import Main from './component/Main';
 import { Link } from 'react-router-dom';
-
 
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="ui container">
-        <Fragment >
-          <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">MyPortfolio</Link>} scroll>
-            <Menu>
-              <Link to="/resume">Resume</Link>
-              <Link to="/aboutme">About Me</Link>
-              <Link to="/projects">Projects</Link>
-              <Link to="/contact">Contact</Link>
-            </Menu>
-          </Header>
+    render() {
+        return (
+            <div className="demo-big-content">
+              <Layout>
+                <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">MyPortfolio</Link>} scroll>
+                  <Navigation>
+                    <Link to="/resume">Resume</Link>
+                    <Link to="/aboutme">About Me</Link>
+                    <Link to="/projects">Projects</Link>
+                    <Link to="/contact">Contact</Link>
+                  </Navigation>
+                </Header>
+                <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">MyPortfolio</Link>}>
+                  <Navigation>
+                    <Link to="/resume">Resume</Link>
+                    <Link to="/aboutme">About Me</Link>
+                    <Link to="/projects">Projects</Link>
+                    <Link to="/contact">Contact</Link>
+                  </Navigation>
+                </Drawer>
+                <Content>
+                  <div className="page-content" />
+                  <Main/>
+                </Content>
+              </Layout>
+            </div>
 
-          <DropdownMenu title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">MyPortfolio</Link>}>
-            <Menu>
-              <Link to="/resume">Resume</Link>
-              <Link to="/aboutme">About Me</Link>
-              <Link to="/projects">Projects</Link>
-              <Link to="/contact">Contact</Link>
-            </Menu>
-          </DropdownMenu>
-
-          <Segment>
-            <div className="page-content" />
-            <Main/>
-          </Segment>
-
-        </Fragment>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
