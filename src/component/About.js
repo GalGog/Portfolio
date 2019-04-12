4654afcbc4fac8afd09700b24f883eabb09f41aa
GalGog <galynna00@gmail.com>
2019-04-12 15:02:27 +0100

    About component - segment

--- src/component/About.js
+++ src/component/About.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react'
-import { Container, Segment, Image, Divider } from 'semantic-ui-react'
+import { Container, Segment, Image } from 'semantic-ui-react'
 import MyLatestWork from "./MyLatestWork";
 import LandingPage from "./LandingPage";
+import Skills from './Skills';
 
 class About extends Component {
     render() {
@@ -15,22 +16,58 @@ class About extends Component {
                     <h1>About</h1>
                     <div className="ui doubling four column grid">
                         <div className="column">
-                            <Image src={src} size='small' verticalAlign='underneath' /> <span><h4>Intuitive</h4> Strong preference for easy to use, intuitive UX/UI.</span>
+                            <Image src={src} size='small' verticalAlign='underneath' /> <span><h4>Fast</h4>Fast load times and lag free interaction, my highest priority.</span>
+                        </div>
+                        <div className="column">
+                            <Image src={src} size='small' verticalAlign='underneath' /> <span><h4>Responsive</h4>My layouts will work on any device, big or small.</span>
                         </div>
                         <div className="column">
                             <Image src={src} size='small' verticalAlign='underneath' /> <span><h4>Intuitive</h4> Strong preference for easy to use, intuitive UX/UI.</span>
                         </div>
                         <div className="column">
-                            <Image src={src} size='small' verticalAlign='underneath' /> <span><h4>Intuitive</h4> Strong preference for easy to use, intuitive UX/UI.</span>
-                        </div>
-                        <div className="column">
-                            <Image src={src} size='small' verticalAlign='underneath' /> <span><h4>Intuitive</h4> Strong preference for easy to use, intuitive UX/UI.</span>
+                            <Image src={src} size='small' verticalAlign='underneath' /> <span><h4>Dynamic</h4>Websites don't have to be static, I love making pages come to life.</span>
                         </div>
                     </div>
                 </Segment>
-
+                <Segment>
+                    <div className="ui doubling two column grid">
+                    <div className="column">
+                        <Image src='/images/wireframe/square-image.png' size='medium' circular />
+                        <span><h4>Intuitive</h4> Strong preference for easy to use, intuitive UX/UI.</span>
+                    </div>
+                    <div className="column">
+                        <Skills
+                            skill="HTML/CSS"
+                            progress={80}
+                        />
+                        <Skills
+                            skill="Bootstrap"
+                            progress={70}
+                        />
+                        <Skills
+                            skill="WordPress"
+                            progress={40}
+                        />
+                        <Skills
+                            skill="JavaScript"
+                            progress={25}
+                        />
+                        <Skills
+                            skill="React/Redux"
+                            progress={25}
+                        />
+                    </div>
+                    </div>
+                </Segment>
                 <Segment>
                     <MyLatestWork/>
+                </Segment>
+                <Segment>
+                    <h1>Blog</h1>
+                </Segment>
+                <Segment>
+                    <h1>Contact</h1>
+                    <p>Have a question or want to work together?</p>
                 </Segment>
         </Container>
         )
