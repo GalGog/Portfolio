9d5dadd05c4408fc5a9bc2d52a744d91372554be
GalGog <galynna00@gmail.com>
2020-05-19 15:55:01 +0100

    initial commit

---
+++ build/precache-manifest.230a06485fbbb8e6a5c4004e6caf9ce3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "4163ac19911fd928353a",
+    "url": "/static/css/main.18e89ea0.chunk.css"
+  },
+  {
+    "revision": "4163ac19911fd928353a",
+    "url": "/static/js/main.858ae722.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1eb55a887cc9639607a6",
+    "url": "/static/css/2.b7edfcfd.chunk.css"
+  },
+  {
+    "revision": "1eb55a887cc9639607a6",
+    "url": "/static/js/2.183332c3.chunk.js"
+  },
+  {
+    "revision": "fa9e9c6085633f7d5286a3fc6c592f1d",
+    "url": "/static/media/LastProject1.fa9e9c60.png"
+  },
+  {
+    "revision": "0e378e31359a91b13147116f76f7b0b7",
+    "url": "/static/media/LastProject3.0e378e31.png"
+  },
+  {
+    "revision": "b956672bd4715e01f327d6c5cf4a90d4",
+    "url": "/static/media/LastProject4.b956672b.png"
+  },
+  {
+    "revision": "eca2c13cb374e9523f775f146dcf2be9",
+    "url": "/static/media/LastProject5.eca2c13c.png"
+  },
+  {
+    "revision": "70fb57a6b4b7af76bdf96a1e72ada849",
+    "url": "/index.html"
+  }
+];
